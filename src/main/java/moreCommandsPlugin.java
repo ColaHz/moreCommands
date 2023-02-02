cfb9c8677cb2dac5537fc58a28d6373285718adf
ColaHz <87813567+ColaHz@users.noreply.github.com>
2023-02-02 16:34:43 -0300

    Update moreCommandsPlugin.java

--- src/main/java/moreCommandsPlugin.java
+++ src/main/java/moreCommandsPlugin.java
@@ -40,7 +40,7 @@ import manager.*;
 public class moreCommandsPlugin extends mindustry.mod.Plugin {
   @SuppressWarnings("unchecked")
   public moreCommandsPlugin() {
-    Log.info("|-> MoreCommands, cargando ....");
+    Log.info("|-> MoreCommands, cargando....");
 
     // init other classes and load settings
     Effects.init();
